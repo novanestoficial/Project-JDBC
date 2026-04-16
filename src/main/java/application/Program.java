92eb85682cf91df05764c9bd04831380ea1a4c96
David Nest <davidcamposoliveira80@gmail.com>
2026-04-16 20:51:51 -0300

    findById implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -10,13 +10,13 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(23, "David", "david@gmail.com", new Date(), 3000.0, obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+
+
+            Seller seller = sellerDao.findById(8);
+
+            System.out.println(seller);
 
     }
 }
