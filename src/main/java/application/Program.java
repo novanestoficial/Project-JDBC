69abe413b90d8e208dca86c114a6b3c3f3998a6f
David Nest <davidcamposoliveira80@gmail.com>
2026-04-16 23:33:51 -0300

    Add methods and aprimorating  the Interface SellerDao and class SellerDaoJDBC, addition methods, lists, and optimization the code

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,10 +14,17 @@ public class Program {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
+        Seller seller = sellerDao.findById(8);
+        System.out.println(seller);
 
-            Seller seller = sellerDao.findById(8);
+        System.out.println("=== TEST 2: seller findById ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
 
-            System.out.println(seller);
+
 
     }
 }
